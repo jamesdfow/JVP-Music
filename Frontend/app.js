ce14588c054733f85b0c6407264513e5c2a8dee0
James <160443991+jamesdfow@users.noreply.github.com>
2024-11-14 06:34:05 -0700

    added extra comments on the JS file for clarity

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,16 +1,18 @@
-//Prevents the form from submitting
+//Waits for the document to fully load before running the script
 document.addEventListener("DOMContentLoaded", function () {
   const searchForm = document.getElementById("searchForm");
+
+  //checks if the search form exists on the page
   if (searchForm) {
     searchForm.addEventListener("submit", function (event) {
-      //prevents form from actually submitting
+      //prevents form from submitting and reloading the page
       event.preventDefault();
       //retrieves search query from the input field
       let searchQuery = document.getElementById("searchQuery").value;
 
-      //storing search query in Local storage
+      //storing search query in Local storage to access on the next page
       localStorage.setItem("searchQuery", searchQuery);
-      //redirecting to page 2
+      //redirecting to page2.html after saving the query
       window.location.href = "page2.html";
     });
   }
